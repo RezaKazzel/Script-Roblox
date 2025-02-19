489d2b69265a7b498be6aaf3a8898901ee1d562f
ja <58966761+RezaKazzel@users.noreply.github.com>
2025-02-20 02:52:41 +0700

    Create Extensions.lua

---
+++ Extension/Extensions.lua
@@ -0,0 +1,7 @@
+--// Extension
+local Rey = loadstring(game:HttpGet("https://raw.githubusercontent.com/RezaKazzel/Script-Roblox/refs/heads/main/UI/Rey%20Library%20New"))()
+
+Extension = Rey:CreateTab(UI, "Extension+")
+Rey:CreateButton(Extension, "Minato", "kilat kuning😹", "Load", function()
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/RezaKazzel/Script-Roblox/refs/heads/main/Extension/Minato.lua"))()
+end)
