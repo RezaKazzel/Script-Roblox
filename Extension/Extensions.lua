4e89b48102e6dfdb3917592274a4883a52cc6d5c
ja <58966761+RezaKazzel@users.noreply.github.com>
2025-12-09 22:01:08 +0700

    Update Extensions.lua

--- Extension/Extensions.lua
+++ Extension/Extensions.lua
@@ -1,11 +1,13 @@
 --// Extension
-local Rey = loadstring(game:HttpGet("https://raw.githubusercontent.com/RezaKazzel/Script-Roblox/refs/heads/main/UI/Rey%20Library%20New"))()
+local Rey = loadstring(game:HttpGet("https://raw.githubusercontent.com/RezaKazzel/Script-Roblox/refs/heads/main/UI/Rey%20Library%20Rework.lua"))()
 
 Extension = Rey:CreateTab(UI, "Extension+")
 Rey:CreateButton(Extension, "Minato", "kilat kuning⚡", "Load", function()
-	loadstring(game:HttpGet("https://raw.githubusercontent.com/RezaKazzel/Script-Roblox/refs/heads/main/Extension/Minato.lua"))()
-end)
-Rey:CreateButton(Extension, "Lua Playground", "hengker💻", "Load", function()
-	loadstring(game:HttpGet("https://raw.githubusercontent.com/RezaKazzel/Script-Roblox/refs/heads/main/Extension/Lua%20Playground.lua"))()
+    Rey:Notify("info", "Extension", "Loading Minato...", 2)
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/RezaKazzel/Script-Roblox/refs/heads/main/Extension/Minato.lua"))()
 end)
 
+Rey:CreateButton(Extension, "Lua Playground", "hengker💻", "Load", function()
+    Rey:Notify("info", "Extension", "Loading Lua Playground...", 2)
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/RezaKazzel/Script-Roblox/refs/heads/main/Extension/Lua%20Playground.lua"))()
+end)
